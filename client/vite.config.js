1fea97640258b6dc48a90730dea07e79a44b5325
Harsh Joshi <114358813+Harshjoshiit@users.noreply.github.com>
2025-08-25 13:39:45 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // This tells Vite's bundler (Rollup) to treat these Node.js modules
-      // as "external". This means it won't try to bundle them for the browser,
-      // which is what causes the build to fail.
-      external: [
-        'fs',
-        'path',
-        'os',
-        'crypto',
-        'stream',
-        'http',
-        'https',
-        'net',
-        'tls',
-        'zlib',
-        'url'
-      ]
-    }
-  }
-});
+})
